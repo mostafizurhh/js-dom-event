2dca90122b2fa6a84d70b9600aac0aadb58012e0
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-08-16 17:38:21 +0100

    inputfield added and updated the data from input field

--- eventlistener.js
+++ eventlistener.js
@@ -9,7 +9,13 @@ document.getElementById('makeBrown').addEventListener('click', function () {
 });
 
 // method 2 -->input field update
-document.getElementById('lightPink').addEventListener('click', function () {
-    document.body.style.backgroundColor = 'lightpink';
-});
 
+document.getElementById('updateButton').addEventListener('click', function () {
+    const inputfield = document.getElementById('inputField');
+    const inputText = inputfield.value;
+
+    const p = document.getElementById('updateParagraph');
+    p.innerText = inputText;
+    const newData = inputText;
+    inputfield.value = '';
+})
